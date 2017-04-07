4038dc9e35049f4d42e4c7bc371e55ef52d2e957
Matt Ruston <matt@rustonmail.com>
2017-04-06 19:54:38 -0500

    Added validation to update to prevent people from cheating

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Vapor
 import VaporPostgreSQL
 
@@ -7,6 +8,8 @@ let drop = Droplet()
 
 try drop.addProvider(VaporPostgreSQL.Provider.self)
 drop.preparations.append(Team.self)
+
+var visitedIPs: [String : (Date, Date, Int)] = [:]
 
 
 // MARK: - Pages
@@ -32,7 +35,43 @@ drop.get("score", Int.self) { req, teamID in
 
 // MARK: - PUT requests
 
-drop.put("update_score", Int.self, Int.self) { _, teamID, score in
+drop.put("update_score", Int.self, Int.self) { request, teamID, score in
+    
+    // Validation logic
+    if let peer = request.peerAddress?.stream {
+        let now = Date()
+        
+        if let old = visitedIPs[peer] {
+            let oldVisit = old.0
+            let lastVisit = old.1
+            let visits = old.2
+            
+            let timeSinceLastRequest = now.timeIntervalSince(oldVisit)
+            
+            if visits >= 10 {
+                // If they have more than 10 requests, wait 30 minutes before resetting them
+                if timeSinceLastRequest > (60.0 * 30.0) {
+                    visitedIPs[peer] = (now, now, 0)
+                } else {
+                    return "Timed out due to too many requests"
+                }
+            } else {
+                if now.timeIntervalSince(lastVisit) < 3, score >= 99 {
+                    visitedIPs[peer] = (now, now, 10)
+                    return "Timed out due to impossible request"
+                }
+                if timeSinceLastRequest > 60.0 {
+                    visitedIPs[peer] = (now, now, 0)
+                } else {
+                    visitedIPs[peer] = (oldVisit, now, visits + 1)
+                }
+            }
+        } else {
+            visitedIPs[peer] = (now, now, 0)
+        }
+    }
+
+    
     if score > 300 || score <= 0 { return "invalid score" }
     
     if var team = try Team.query().filter("teamid", teamID).first() {
